262fe0dd594ab4c12703da0bd75ed54cda1123da
SSOL <livingsnow011@gmail.com>
2021-05-31 23:38:19 +0900

    2021-05-31

--- practice.py
+++ practice.py
@@ -120,3 +120,32 @@ data="039490       "; data=data.rstrip();print(data)
 리스트
 '''
 #051
+movie_rank=["닥터 스트레인지","스플릿","럭키"]
+#052
+movie_rank.append("배트맨")
+#053
+movie_rank.insert(1,"슈퍼맨")
+#054
+del movie_rank[3]
+#055
+del movie_rank[2]
+del movie_rank[2]
+#056
+lang1=["c","c++","JAVA"]
+lang2=["python","Go","c#"]
+langs=lang1+lang2
+#057
+nums=[1,2,3,4,5,6,7]
+print(max(nums),min(nums))
+#058
+nums=[1,2,3,4,5]
+print(sum(nums))
+#059
+cook = ["피자", "김밥", "만두", "양념치킨", 
+"족발", "피자", "김치만두", "쫄면", "쏘세지", "라면", "팥빙수", "김치전"]
+print(len(cook))
+#060
+nums=[1,2,3,4,5]
+print(sum(nums)/len(nums))
+#061
+
