06db5fb65263c13d08a0e3e2dd133ac36a76d5ff
SOL <83905764+livingsnow011@users.noreply.github.com>
2022-01-26 22:24:57 +0900

    Delete PyNet directory

--- PyNet/8_broadcast_server.py
+++
@@ -1,9 +0,0 @@
-from socket import *
-
-sock = socket(AF_INET,SOCK_DGRAM) # UDP, 브로드캐스트
-sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1) # 주소 재사용
-sock.bind(("",10000))
-
-while True:
-    msg,addr = sock.recvfrom(1024)
-    print(msg.decode())
