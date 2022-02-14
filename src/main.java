1ead12d1b4a4355c60d9f4fd2a54dc4395d21d37
SOL <livingsnow011@gmail.com>
2022-02-14 23:35:16 +0900

    time complexity

--- src/main.java
+++ src/main.java
@@ -1,5 +1,9 @@
 public class main {
     public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+
         System.out.println("main.main");
+        long end = System.currentTimeMillis();
+        System.out.println((end - start)/1000.0);
     }
 }
