e2721a5d3725c495a11e679d0a5b198a1273d3c7
SOL <livingsnow011@gmail.com>
2022-04-28 23:41:10 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,14 @@
 import java.io.*;
 import java.util.StringTokenizer;
 
-class Main{
+class Main {
+
     public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = null;
         // 여기까지가 입력
-        int T = Integer.parseInt(br.readLine());
-        int[][] apt = new int[15][15];
-        for (int i = 0; i < 15; ++i) {
-            apt[i][1] = 1;
-            apt[0][i] = i;
-        }
-        for(int i=0;i<T;++i){
-            int k = Integer.parseInt(br.readLine());
-            int n = Integer.parseInt(br.readLine());
 
-
-        }
         //여기부터가 출력
         bw.flush();
         bw.close();
